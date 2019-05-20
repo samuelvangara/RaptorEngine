911bce90a3c3eccd06291a891fa48b6aacd73e76
SamuelVangara <samuelvangara@gmail.com>
2019-05-20 13:33:22 -0400

    Spring backend and Angular front end initialization code

---
+++ ExpenseReportWebApplication/expenseReportBackendApplication/src/main/java/reptorEngine/expenseReportBackendApplication/ExpenseReportApplication.java
@@ -0,0 +1,13 @@
+package reptorEngine.expenseReportBackendApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReportApplication.class, args);
+	}
+
+}
