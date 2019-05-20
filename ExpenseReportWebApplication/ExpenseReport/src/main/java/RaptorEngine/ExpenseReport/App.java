9bab335ecbb365fe2285360b15ef4c1c9b856696
SamuelVangara <samuelvangara@gmail.com>
2019-05-20 01:50:46 -0400

    Expense report divided into three sub projects in the alpha branch containing A Web, Android and IOS application

---
+++ ExpenseReportWebApplication/ExpenseReport/src/main/java/RaptorEngine/ExpenseReport/App.java
@@ -0,0 +1,13 @@
+package RaptorEngine.ExpenseReport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
